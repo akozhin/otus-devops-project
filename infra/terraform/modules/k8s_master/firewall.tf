f13f917d7f0f1757b413e96e98d2d75824ea963c
Maksim Soldatov <mnsold.otus@gmail.com>
2019-07-07 22:05:42 +0300

    add k8s firewall

--- infra/terraform/modules/k8s_master/firewall.tf
+++ infra/terraform/modules/k8s_master/firewall.tf
@@ -16,3 +16,22 @@ resource "google_compute_firewall" "k8s_master_http" {
   # Правило применимо для инстансов с перечисленными тэгами
   target_tags = ["${var.instance_tag}"]
 }
+
+resource "google_compute_firewall" "k8s_master_6443" {
+  name = "allow-${var.instance_name}-6443"
+
+  # Название сети, в которой действует правило
+  network = "${var.instance_network_interface}"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["6443"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["${var.instance_tag}"]
+}
